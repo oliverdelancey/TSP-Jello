5f58cca42f5eedb3b05a270c86a6342284d8d183
1uC3 <147186536+1uC3@users.noreply.github.com>
2023-12-03 22:14:48 -0500

    Returning userauth.php to before today's edits
    Failed to make registration work as a part of this. The page would not redirect upon edits made. Will try again later.

--- userauth.php
+++ userauth.php
@@ -1,57 +1,50 @@
 <html>
 <body>
 <?php
-var_dump($_POST);
 include "database.php";
-include "db.php";
+session_start();
 #session_start();
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-$conn = new mysqli($server, $username, $password, $database);
-if ($conn->connect_error) {
-	die("connection failed: " . $conn->connect_error);
+@@ -14,7 +14,7 @@
 }
 
-$valid_input = true;
-
 $uname = $_POST["uname"];
+$_SESSION["uname"]=$_POST["uname"];
 #$_SESSION["uname"] = $_POST["uname"];
-
+$valid_input = true;
 if (empty($uname)) {
 	echo "Username field is empty";
 	$valid_input = false;
 }
-
 $password = $_POST["password"];
-
 if (empty($password)) {
 	echo "password field is empty";
 	$valid_input = false;
 }
-
 if ($valid_input) {
-
-	echo("validated");
-	if(isset($_POST["submit"])) {
+	$query = $conn->prepare("SELECT username, password FROM users WHERE username = ?");
+	$query->bind_param("s", $uname);
+	$query->execute();
+	$result = $query->get_result();
+	$isSuccess = false;
+	while ($row = $result->fetch_assoc()) {
+		if (!empty($row)) {
+			$hpassword = $row["password"];
+			if ($password == $hpassword) {
+				$isSuccess = true;
+			}
+		}
 	}
-	if($_POST["submit"]=='Log in') {
-		#authenticate_user($uname, $password);
-		header('Location: userHome.php');
+	if ($isSuccess) {
+		echo "login successful";
+		header("LOCATION: userHome.php");
+	} else {
+		echo "login failed";
 	}
-	
-	if($_POST["submit"]=='Create User') {
-		#create_user($uname, $password);
-		header('Location: simplelogin.html');
-	}
-	
 }
-
-header('Location: index.html');
 $conn->close();
-
 ?>
 </body>
 </html>
