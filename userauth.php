a36b190ce6fd4cb3c056039f0f42739246f513bd
1uC3 <147186536+1uC3@users.noreply.github.com>
2023-12-03 22:03:44 -0500

    Debugging #8 authuser.php redirects

--- userauth.php
+++ userauth.php
@@ -6,36 +6,24 @@ include "database.php";
 include "db.php";
 #session_start();
 
-echo("here1");
-
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
-echo("here2");
 
 $conn = new mysqli($server, $username, $password, $database);
 if ($conn->connect_error) {
 	die("connection failed: " . $conn->connect_error);
 }
 
-echo("here3");
-
 $valid_input = true;
-
-echo("here4");
 
 $uname = $_POST["uname"];
 #$_SESSION["uname"] = $_POST["uname"];
-
-echo("here5");
 
 if (empty($uname)) {
 	echo "Username field is empty";
 	$valid_input = false;
 }
-
-echo("here6");
 
 $password = $_POST["password"];
 
@@ -44,32 +32,24 @@ if (empty($password)) {
 	$valid_input = false;
 }
 
-echo("here7");
-
 if ($valid_input) {
 
 	echo("validated");
 	if(isset($_POST["submit"])) {
-
-		echo("submitted");
 	}
 	if($_POST["submit"]=='Log in') {
-		echo("Logged in");
 		#authenticate_user($uname, $password);
 		header('Location: userHome.php');
 	}
 	
 	if($_POST["submit"]=='Create User') {
-		echo("Created");
 		#create_user($uname, $password);
 		header('Location: simplelogin.html');
 	}
-
-	echo("Not redirected");
-	header('Location: index.html');
-	exit();
+	
 }
-echo("not valid");
+
+header('Location: index.html');
 $conn->close();
 
 ?>
