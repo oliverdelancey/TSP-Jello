45d1c9b0c6bf7626388d0ce9571e4b7048e07341
1uC3 <147186536+1uC3@users.noreply.github.com>
2023-12-04 20:56:30 -0500

    Creating login.php to replace simplelogin.html for invalid login visual effects

---
+++ login.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="styles.css">
+    </head>
+
+    <body>
+          
+        <ul>
+            <li class="h1" style="font-size:30px; font-weight:600">Jello</li>
+
+            <li class="active" style="float:right"><a href="http://syenite.net/login.php">Sign In</a></li>
+            <li style="float:right"><a href="http://syenite.net/register.html">Register</a></li>
+            
+        </ul>
+
+        <div class="content">
+            <div class="container">
+    
+                <h1>Jello Sign In</h1>
+                <form method="post" action="userauth.php">
+        
+                    <label for="uname">Username:</label><br>
+                    <p><input type="text" id="uname" name="uname" placeholder="Type your username"
+                            onClick="this.value='';"><br></p>
+        
+                    <label for="password">Password:</label><br>
+                    <input type="password" id="password" name="password" placeholder="Type your password"
+                            onClick="this.value='';"><br><br>
+
+                    <php
+                      if(isset($_GET['error'])) {
+                          echo('<p style=color:red>Invalid username or password</p>');
+                      }
+                    ?>
+                    
+                    <input type="submit" name="submit" id="submit" value="Log in"><br>
+                </form>
+                <label for="register" id="register"><p>Don't have an account? <a href="http://syenite.net/register.html">Register</a></p></label>
+            </div>
+        </div>
+
+    </body>
+
+  <footer>
+  </footer>
+
+</html>
