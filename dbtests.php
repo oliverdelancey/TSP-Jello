6f4d53e75b8ab34ebea2a56ecf5d6f64ab48590e
Oliver Delancey <oliverdelancey@gmail.com>
2023-12-05 20:03:30 +0000

    Set up testing framework

--- dbtests.php
+++ dbtests.php
@@ -3,17 +3,29 @@
 <title>DB PHP Tests</title>
 </head>
 <body>
+
 <?php
 
 include "db.php";
 
-var_dump($_SESSION);
+//var_dump($_SESSION);
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-try{
+function heading($text) {
+    echo "<h3>";
+    echo $text;
+    echo "</h3>";
+}
+
+function print_array($array) {
+    echo "<pre>"; print_r($array); echo "</pre>";
+    echo "<br/>";
+}
+
+try {
     $statement = $conn->prepare(
         "select id from users where username = ?"
     );
@@ -21,16 +33,55 @@ try{
     $statement->bind_param("d", $_SESSION["uname"]);
 
     $statement->execute(); 
-    $result = $statement->get_result();
-    $row = $statement->fetch();
-    $userid = $row[0];
+    $statement->bind_result($userid);
+    $statement->fetch();
+    echo "Got userid: " . $userid . "<br/>";
+    $statement->close();
 
-    // tests
+    /* TESTS */
 
+    // get_projects
     $projects = get_projects($userid);
-    echo "<p>function get_projects: " . $projects . "</p>";
+    heading("function get_projects");
+    print_array($projects);
 
-} catch(PDOException $e){
+    // get_columns
+    $project_id = $projects[0][3];
+    $columns = get_columns($project_id);
+    heading("function get_columns");
+    print_array($columns);
+
+    // get_tasks
+
+    // get_collaborators
+
+    // create_project
+
+    // create_column
+
+    // create_task
+    
+    // create_user
+
+    // delete_project
+
+    // delete_column
+
+    // delete_task
+
+    // modify_task
+
+    // modify_project
+
+    // modify_column
+
+    // modify_task_assignment
+
+    // create_project_assignment
+
+    // delete_project_assignment
+
+} catch(PDOException $e) {
     print "Error!" . $e->getMessage() . "<br/>"; 
     die(); 
 }
