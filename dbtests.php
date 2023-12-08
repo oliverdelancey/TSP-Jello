d7dac613c0b133ecc7af2fd9ef4fb4466ac2d8d8
Oliver Delancey <oliverdelancey@gmail.com>
2023-12-08 16:22:14 +0000

    Set up the rest of the tests

--- dbtests.php
+++ dbtests.php
@@ -55,34 +55,80 @@ try {
     print_array($columns);
 
     // get_tasks
+    $tasks = get_tasks(0);
+    heading("function get_tasks");
+    print_array($tasks);
 
     // get_collaborators
+    $collaborators = get_collaborators(0);
+    heading("function get_collaborators");
+    print_array($collaborators);
 
     // create_project
+    $status = create_project("test", "test", 0, 0, 0);
+    heading("function create_project");
+    print_array($status);
 
     // create_column
+    $status = create_column("test_col", 0);
+    heading("function create_column");
+    print_array($status);
 
     // create_task
+    $status = create_task(0, 0, 3, "test task");
+    heading("function create_task");
+    print_array($status);
     
     // create_user
-
-    // delete_project
-
-    // delete_column
-
-    // delete_task
+    $status = create_user("sam", "smith");
+    heading("function create_user");
+    print_array($status);
 
     // modify_task
+    $result = modify_task(0, 2, "why", "status", 0);
+    heading("function modify_task");
+    print_array($result);
 
     // modify_project
+    $result = modify_project("test", 5, 5, 0, "why");
+    heading("function modify_project");
+    print_array($result);
 
     // modify_column
+    $result = modify_column(0, "testing");
+    heading("function modify_column");
+    print_array($result);
 
     // modify_task_assignment
+    $result = modify_task_assignment(0, 0);
+    heading("function modify_task_assignment");
+    print_array($result);
 
     // create_project_assignment
+    $result = create_project_assignment(0, 0);
+    heading("function create_project_assignment");
+    print_array($result);
 
     // delete_project_assignment
+    $result = delete_project_assignment(0, 0);
+    heading("function delete_project_assignment");
+    print_array($result);
+
+    // delete_project
+    $result = delete_project(0);
+    heading("function delete_project");
+    print_array($result);
+
+    // delete_column
+    $result = delete_column(0, 0);
+    heading("function delete_column");
+    print_array($result);
+
+    // delete_task
+    $result = delete_task(0);
+    heading("function delete_task");
+    print_array($result);
+
 
     $conn->commit();
     $committed = true;
